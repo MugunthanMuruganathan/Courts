d2615ba02e3fa7fe9f2fcb1558874aca55b32bac
MugunthanMuruganathan <mugunthan.muruganathan@gmail.com>
2025-08-18 15:27:50 +0530

    Trying to print the table

--- model_definitions/r-courts/model_modules/evaluation.R
+++ model_definitions/r-courts/model_modules/evaluation.R
@@ -34,6 +34,7 @@ evaluate <- function(data_conf, model_conf, ...) {
   preds <- as.integer(ifelse(probs > 0.5, 1, 0))
 
   print("Before Confustion Matrix call...")
+  print table(preds, data$Reverse) 
   
   cm <- confusionMatrix(table(preds, data$Reverse))
 
