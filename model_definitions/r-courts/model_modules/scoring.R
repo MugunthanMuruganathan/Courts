c053256f0c5b2a31a71fa3bb7320906457ccb8b8
MugunthanMuruganathan <mugunthan.muruganathan@gmail.com>
2025-08-20 17:29:40 +0530

    model name is changed

--- model_definitions/r-courts/model_modules/scoring.R
+++ model_definitions/r-courts/model_modules/scoring.R
@@ -75,5 +75,5 @@ score.batch <- function(data_conf, model_conf, model_version, job_id, ...) {
 
 initialise_model <- function() {
     print("Loading model...")
-    model <- readRDS("artifacts/input/model.rds")
+    model <- readRDS("artifacts/input/court_model.rds")
 }
