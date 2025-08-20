4ae7c26c8ef6858d02e992e208ea1b8bc480accd
MugunthanMuruganathan <mugunthan.muruganathan@gmail.com>
2025-08-20 17:23:29 +0530

    scoring file is changed

--- model_definitions/r-courts/model_modules/scoring.R
+++ model_definitions/r-courts/model_modules/scoring.R
@@ -29,12 +29,18 @@ score.batch <- function(data_conf, model_conf, model_version, job_id, ...) {
     table <- tbl(con, sql(data_conf$sql))
 
     # Create dataframe from tibble, selecting the necessary columns and mutating integer64 to integers
-    data <- table %>% mutate(PatientId = as.integer(PatientId),
-                             NumTimesPrg = as.integer(NumTimesPrg),
-                             PlGlcConc = as.integer(PlGlcConc),
-                             BloodP = as.integer(BloodP),
-                             SkinThick = as.integer(SkinThick),
-                             TwoHourSerIns = as.integer(TwoHourSerIns)) %>% as.data.frame()
+    data <- table %>% mutate(
+							  DocketId = as.integer(DocketId),
+							  Docket = as.integer(Docket),
+							  Term = as.integer(Term),
+							  Circuit = as.integer(Circuit),
+							  Issue = as.integer(Issue),
+							  Petitioner = as.integer(Petitioner),
+							  Respondent = as.integer(Respondent),
+							  LowerCourt = as.integer(LowerCourt),
+							  Uncon = as.integer(Uncon),
+							  Reverse = as.integer(Reverse)
+							 ) %>% as.data.frame()
 
     # The model object will be obtain from the environment as it has already been initialised using 'initialise_model'
     probs <- predict(model, data, na.action = na.pass, type = "response")
@@ -43,8 +49,8 @@ score.batch <- function(data_conf, model_conf, model_version, job_id, ...) {
 
     # create result dataframe and store in Teradata Vantage
     pred_df <- as.data.frame(unlist(score))
-    colnames(pred_df) <- c("HasDiabetes")
-    pred_df$PatientId <- data$PatientId
+    colnames(pred_df) <- c("Reverse")
+    pred_df$DocketId <- data$DocketId
     pred_df$job_id <- job_id
 
     # tdplyr doesn't match column names on append.. and so to match / use same table schema as for byom predict
@@ -52,13 +58,13 @@ score.batch <- function(data_conf, model_conf, model_version, job_id, ...) {
     # CREATE MULTISET TABLE pima_patient_predictions
     # (
     #     job_id VARCHAR(255), -- comes from airflow on job execution
-    #     PatientId BIGINT,    -- entity key as it is in the source data
-    #     HasDiabetes BIGINT,   -- if model automatically extracts target
+    #     DocketId BIGINT,    -- entity key as it is in the source data
+    #     Reverse BIGINT,   -- if model automatically extracts target
     #     json_report CLOB(1048544000) CHARACTER SET UNICODE  -- output of
     # )
     # PRIMARY INDEX ( job_id );
     pred_df$json_report <- ""
-    pred_df <- pred_df[, c("job_id", "PatientId", "HasDiabetes", "json_report")]
+    pred_df <- pred_df[, c("job_id", "DocketId", "Reverse", "json_report")]
 
     copy_to(con, pred_df,
             name=dbplyr::in_schema(data_conf$predictions$database, data_conf$predictions$table),
